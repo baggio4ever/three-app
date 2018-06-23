3591ae5cd3376772f54f040b0d0aa2d315ed632e
baggio4ever <baggio4ever@gmail.com>
2018-06-23 15:55:01 +0900

    ・球の描画、テクスチャのサンプルコード追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent implements OnInit {
   ngOnInit() {
 		// this.creating_a_scene();
 		// this.drawing_lines();
-		this.draw_cube();
+		// this.draw_cube();
+		this.draw_shpere();
 	}
 	
 	// Getting Started: Creating a scene
@@ -119,6 +120,62 @@ export class AppComponent implements OnInit {
 
 			requestAnimationFrame(tick);
 		}
+	}
+
+	// https://ics.media/tutorial-three/material_basic.html
+	// http://planetpixelemporium.com/earth.html
+	draw_shpere() {
+		// レンダラーを作成
+		const renderer = new THREE.WebGLRenderer();
+		renderer.setSize( THREE_WIDTH, THREE_HEIGHT );
+		const el = document.getElementById('three');
+		el.appendChild( renderer.domElement );
+
+
+		// シーンを作成
+		const scene = new THREE.Scene();
+		
+		// カメラを作成
+		const camera = new THREE.PerspectiveCamera(45, THREE_WIDTH / THREE_HEIGHT, 1, 10000);
+		camera.position.set(0, 0, +1000);
+		
+		// 球体を作成
+		const geometry = new THREE.SphereGeometry(300, 30, 30);
+
+		// 画像を読み込む
+		const loader = new THREE.TextureLoader();
+		const texture = loader.load('../assets/images/earthmap1k.jpg');
+		// const texture = loader.load('../assets/images/earthlights1k.jpg');
+		// マテリアルにテクスチャーを設定
+		const material = new THREE.MeshStandardMaterial({
+			map: texture
+		});
+/*
+		// 単色
+		const material = new THREE.MeshStandardMaterial({color: 0xFF0000});
+*/
+		  // メッシュを作成
+		  const mesh = new THREE.Mesh(geometry, material);
+		  // 3D空間にメッシュを追加
+		  scene.add(mesh);
+		
+		  // 平行光源
+		  const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+		  directionalLight.position.set(1, 1, 1);
+		  // シーンに追加
+		  scene.add(directionalLight);
+		
+		  tick();
+		
+		  // 毎フレーム時に実行されるループイベントです
+		  function tick() {
+			// メッシュを回転させる
+			mesh.rotation.y += 0.01;
+			// レンダリング
+			renderer.render(scene, camera);
+		
+			requestAnimationFrame(tick);
+		  }
 	}
 }
 
